8101ca3c2f5bf61d039625850811a0d5ac301100
Viswas-Programs <115633223+Viswas-Programs@users.noreply.github.com>
2023-09-15 12:43:45 +0000

    repl stuff lol

--- client/src/game.ts
+++ client/src/game.ts
@@ -15,8 +15,8 @@ import { cookieExists, getCookieValue } from "cookies-utils";
 
 //handle users that tried to go to old domain name, or direct ip
 var urlargs = new URLSearchParams(window.location.search);
-if(urlargs.get("from")){
-	alert("We have moved from " + urlargs.get("from") + " to islandr.io!")
+if (urlargs.get("from")) {
+  alert("We have moved from " + urlargs.get("from") + " to islandr.io!")
 }
 
 export var world: World;
@@ -42,221 +42,221 @@ var ws: WebSocket;
 var connected = false;
 
 async function init(address: string) {
-	// Initialize the websocket
-	var protocol = "ws";
-	// if ((<HTMLInputElement>document.getElementById("wss")).checked) protocol += "s";
-	ws = new WebSocket(`${protocol}://${address}`);
-	ws.binaryType = "arraybuffer";
+  // Initialize the websocket
+  var protocol = "wss";
+  // if ((<HTMLInputElement>document.getElementById("wss")).checked) protocol += "s";
+  ws = new WebSocket(`${protocol}://${address}`);
+  ws.binaryType = "arraybuffer";
 
-	await new Promise((res, rej) => {
-		const timer = setTimeout(() => {
-			rej(new Error("Failed finding game"));
-			ws.close();
-		}, TIMEOUT);
+  await new Promise((res, rej) => {
+    const timer = setTimeout(() => {
+      rej(new Error("Failed finding game"));
+      ws.close();
+    }, TIMEOUT);
 
-		ws.onmessage = async (event) => {
-			const data = <AckPacket>receive(event.data);
-			id = data.id;
-			tps = data.tps;
-			world = new World(new Vec2(data.size[0], data.size[1]), castCorrectTerrain(data.terrain));
-	
-			// Call renderer start to setup
-			await start();
-			var currentCursor = localStorage.getItem("selectedCursor")
-			if (!currentCursor){localStorage.setItem("selectedCursor", "default"); currentCursor = localStorage.getItem("selectedCursor")}
-			if (currentCursor) {document.documentElement.style.cursor = currentCursor}
-			console.log("from game.ts client skin! > " + skin! + " and death img > " + deathImg!)
-			send(ws, new ResponsePacket(id, username!, skin!, deathImg!, cookieExists("gave_me_cookies") ? getCookieValue("access_token") : getToken()!));
-			connected = true;
-			clearTimeout(timer);
-			
-			// Setup healing items click events
-			for (const element of document.getElementsByClassName("healing-panel")) {
-				const el = <HTMLElement> element;
-				console.log("Adding events for", el.id);
-				const ii = parseInt(<string>el.id.split("-").pop());
-				el.onmouseenter = el.onmouseleave = () => toggleMouseDisabled();
-				el.onclick = () => {
-					if (!el.classList.contains("enabled")) return;
-					send(ws, new UseHealingPacket(Healing.mapping[ii]));
-				}
-			}
-	
-			const interval = setInterval(() => {
-				if (connected) send(ws, new PingPacket());
-				else clearInterval(interval);
-			}, 1000);
-	
-			ws.onmessage = (event) => {
-				const data = receive(event.data);
-				switch (data.type) {
-					case "game": {
-						const gamePkt = <GamePacket>data;
-						world.updateEntities(gamePkt.entities, gamePkt.discardEntities);
-						world.updateObstacles(gamePkt.obstacles, gamePkt.discardObstacles);
-						world.updateLiveCount(gamePkt.alivecount);
-						if (gamePkt.safeZone) world.updateSafeZone(gamePkt.safeZone);
-						if (gamePkt.nextSafeZone) world.updateNextSafeZone(gamePkt.nextSafeZone);
-						if (!player) player = new FullPlayer(gamePkt.player);
-						else player.copy(gamePkt.player);
-						break;
-					}
-					case "map": {
-						// This should happen once only normally
-						const mapPkt = <MapPacket>data;
-						world.terrains = mapPkt.terrains.map(ter => castCorrectTerrain(ter));
-						world.obstacles = mapPkt.obstacles.map(obs => castCorrectObstacle(castMinObstacle(obs)));
-						world.buildings = mapPkt.buildings.map(bui => new Building(bui));
-						initMap();
-						//Show player count once game starts
-						(document.querySelector("#playercountcontainer") as HTMLInputElement).style.display = "block";
-						break;
-					}
-					case "sound": {
-						if (!player) break;
-						const soundPkt = <SoundPacket>data;
-						const howl = new Howl({
-							src: `assets/sounds/${soundPkt.path}`
-						});
-						const pos = Vec2.fromMinVec2(soundPkt.position);
-						const relative = pos.addVec(player.position.inverse()).scaleAll(1/60);
-						howl.pos(relative.x, relative.y);
-						const id = howl.play();
-						world.sounds.set(id, { howl, pos });
-						howl.on("end", () => world.sounds.delete(id));
-						break;
-					}
-				}
-			}
-		}
-	
-		// Reset everything when connection closes
-		ws.onclose = () => {
-			connected = false;
-			stop();
-			Howler.stop();
-			id = null;
-			tps = 1;
-			username = null;
-			player = null;
-			res(undefined);
-			//remove playercount
-		}
-	
-		ws.onerror = (err) => {
-			console.error(err);
-			rej(new Error("Failed joining game"));
-		};
-	});
+    ws.onmessage = async (event) => {
+      const data = <AckPacket>receive(event.data);
+      id = data.id;
+      tps = data.tps;
+      world = new World(new Vec2(data.size[0], data.size[1]), castCorrectTerrain(data.terrain));
+
+      // Call renderer start to setup
+      await start();
+      var currentCursor = localStorage.getItem("selectedCursor")
+      if (!currentCursor) { localStorage.setItem("selectedCursor", "default"); currentCursor = localStorage.getItem("selectedCursor") }
+      if (currentCursor) { document.documentElement.style.cursor = currentCursor }
+      console.log("from game.ts client skin! > " + skin! + " and death img > " + deathImg!)
+      send(ws, new ResponsePacket(id, username!, skin!, deathImg!, cookieExists("gave_me_cookies") ? getCookieValue("access_token") : getToken()!));
+      connected = true;
+      clearTimeout(timer);
+
+      // Setup healing items click events
+      for (const element of document.getElementsByClassName("healing-panel")) {
+        const el = <HTMLElement>element;
+        console.log("Adding events for", el.id);
+        const ii = parseInt(<string>el.id.split("-").pop());
+        el.onmouseenter = el.onmouseleave = () => toggleMouseDisabled();
+        el.onclick = () => {
+          if (!el.classList.contains("enabled")) return;
+          send(ws, new UseHealingPacket(Healing.mapping[ii]));
+        }
+      }
+
+      const interval = setInterval(() => {
+        if (connected) send(ws, new PingPacket());
+        else clearInterval(interval);
+      }, 1000);
+
+      ws.onmessage = (event) => {
+        const data = receive(event.data);
+        switch (data.type) {
+          case "game": {
+            const gamePkt = <GamePacket>data;
+            world.updateEntities(gamePkt.entities, gamePkt.discardEntities);
+            world.updateObstacles(gamePkt.obstacles, gamePkt.discardObstacles);
+            world.updateLiveCount(gamePkt.alivecount);
+            if (gamePkt.safeZone) world.updateSafeZone(gamePkt.safeZone);
+            if (gamePkt.nextSafeZone) world.updateNextSafeZone(gamePkt.nextSafeZone);
+            if (!player) player = new FullPlayer(gamePkt.player);
+            else player.copy(gamePkt.player);
+            break;
+          }
+          case "map": {
+            // This should happen once only normally
+            const mapPkt = <MapPacket>data;
+            world.terrains = mapPkt.terrains.map(ter => castCorrectTerrain(ter));
+            world.obstacles = mapPkt.obstacles.map(obs => castCorrectObstacle(castMinObstacle(obs)));
+            world.buildings = mapPkt.buildings.map(bui => new Building(bui));
+            initMap();
+            //Show player count once game starts
+            (document.querySelector("#playercountcontainer") as HTMLInputElement).style.display = "block";
+            break;
+          }
+          case "sound": {
+            if (!player) break;
+            const soundPkt = <SoundPacket>data;
+            const howl = new Howl({
+              src: `assets/sounds/${soundPkt.path}`
+            });
+            const pos = Vec2.fromMinVec2(soundPkt.position);
+            const relative = pos.addVec(player.position.inverse()).scaleAll(1 / 60);
+            howl.pos(relative.x, relative.y);
+            const id = howl.play();
+            world.sounds.set(id, { howl, pos });
+            howl.on("end", () => world.sounds.delete(id));
+            break;
+          }
+        }
+      }
+    }
+
+    // Reset everything when connection closes
+    ws.onclose = () => {
+      connected = false;
+      stop();
+      Howler.stop();
+      id = null;
+      tps = 1;
+      username = null;
+      player = null;
+      res(undefined);
+      //remove playercount
+    }
+
+    ws.onerror = (err) => {
+      console.error(err);
+      rej(new Error("Failed joining game"));
+    };
+  });
 }
 
 document.getElementById("connect")?.addEventListener("click", async () => {
-	const errorText = <HTMLDivElement>document.getElementById("error-div");
-	username = (<HTMLInputElement>document.getElementById("username")).value;
-	address = (<HTMLInputElement>document.getElementById("address")).value;
-	try {
-		check(username, address);
-		await init(address);
-		errorText.style.display = "none";
-	} catch (error: any) {
-		errorText.innerHTML = error.message;
-		errorText.style.display = "block";
-		return;
-	}
+  const errorText = <HTMLDivElement>document.getElementById("error-div");
+  username = (<HTMLInputElement>document.getElementById("username")).value;
+  address = (<HTMLInputElement>document.getElementById("address")).value;
+  try {
+    check(username, address);
+    await init(address);
+    errorText.style.display = "none";
+  } catch (error: any) {
+    errorText.innerHTML = error.message;
+    errorText.style.display = "block";
+    return;
+  }
 });
 
 function check(username: string, address: string): Error | void {
-	if (!username)
-		throw new Error("Please provide a username.");
-	else if (username.length > 50)
-		throw new Error("Username too long! Try another username.");
+  if (!username)
+    throw new Error("Please provide a username.");
+  else if (username.length > 50)
+    throw new Error("Username too long! Try another username.");
 
-	if (!address)
-		throw new Error("Please provide an address.");
+  if (!address)
+    throw new Error("Please provide an address.");
 }
 
 document.getElementById("disconnect")?.addEventListener("click", () => {
-	ws.close();
-	document.getElementById("settings")?.classList.add("hidden");
-	toggleMenu();
+  ws.close();
+  document.getElementById("settings")?.classList.add("hidden");
+  toggleMenu();
 });
 
 window.onkeydown = (event) => {
-	if (!connected || isKeyPressed(event.key)) return;
-	event.stopPropagation();
-	addKeyPressed(event.key);
-	const settingsElem = document.getElementById("settings");
-	if (event.key == KeyBind.MENU) {
-		if (isMenuHidden()) settingsElem?.classList.remove("hidden");
-		else settingsElem?.classList.add("hidden");
-		toggleMenu();
-	} else if (event.key == KeyBind.HIDE_HUD) toggleHud();
-	else if (event.key == KeyBind.WORLD_MAP) toggleMap();
-	else if (event.key == KeyBind.HIDE_MAP) toggleMinimap();
-	else if (event.key == KeyBind.BIG_MAP) toggleBigMap();
-	if (isMenuHidden()) {
-		const index = movementKeys.indexOf(event.key);
-		if (index >= 0)
-			send(ws, new MovementPressPacket(index));
-		else if (event.key == KeyBind.INTERACT)
-			send(ws, new InteractPacket());
-		else if (event.key == KeyBind.RELOAD)
-			send(ws, new ReloadWeaponPacket());
-		else if (!isNaN(parseInt(event.key)))
-			send(ws, new SwitchWeaponPacket(parseInt(event.key) - 1, true));
-	}
+  if (!connected || isKeyPressed(event.key)) return;
+  event.stopPropagation();
+  addKeyPressed(event.key);
+  const settingsElem = document.getElementById("settings");
+  if (event.key == KeyBind.MENU) {
+    if (isMenuHidden()) settingsElem?.classList.remove("hidden");
+    else settingsElem?.classList.add("hidden");
+    toggleMenu();
+  } else if (event.key == KeyBind.HIDE_HUD) toggleHud();
+  else if (event.key == KeyBind.WORLD_MAP) toggleMap();
+  else if (event.key == KeyBind.HIDE_MAP) toggleMinimap();
+  else if (event.key == KeyBind.BIG_MAP) toggleBigMap();
+  if (isMenuHidden()) {
+    const index = movementKeys.indexOf(event.key);
+    if (index >= 0)
+      send(ws, new MovementPressPacket(index));
+    else if (event.key == KeyBind.INTERACT)
+      send(ws, new InteractPacket());
+    else if (event.key == KeyBind.RELOAD)
+      send(ws, new ReloadWeaponPacket());
+    else if (!isNaN(parseInt(event.key)))
+      send(ws, new SwitchWeaponPacket(parseInt(event.key) - 1, true));
+  }
 }
 
 window.onkeyup = (event) => {
-	if (!connected) return;
-	event.stopPropagation();
-	removeKeyPressed(event.key);
-	const index = movementKeys.indexOf(event.key);
-	if (index >= 0)
-		send(ws, new MovementReleasePacket(index));
+  if (!connected) return;
+  event.stopPropagation();
+  removeKeyPressed(event.key);
+  const index = movementKeys.indexOf(event.key);
+  if (index >= 0)
+    send(ws, new MovementReleasePacket(index));
 }
 
 window.onmousemove = (event) => {
-	if (!connected) return;
-	event.stopPropagation();
-	send(ws, new MouseMovePacket(event.x - window.innerWidth / 2, event.y - window.innerHeight / 2));
+  if (!connected) return;
+  event.stopPropagation();
+  send(ws, new MouseMovePacket(event.x - window.innerWidth / 2, event.y - window.innerHeight / 2));
 }
 
 window.onmousedown = (event) => {
-	if (!connected || isMouseDisabled()) return;
-	event.stopPropagation();
-	addMousePressed(event.button);
-	send(ws, new MousePressPacket(event.button));
+  if (!connected || isMouseDisabled()) return;
+  event.stopPropagation();
+  addMousePressed(event.button);
+  send(ws, new MousePressPacket(event.button));
 }
 
 window.onmouseup = (event) => {
-	if (!connected) return;
-	event.stopPropagation();
-	removeMousePressed(event.button);
-	send(ws, new MouseReleasePacket(event.button));
+  if (!connected) return;
+  event.stopPropagation();
+  removeMousePressed(event.button);
+  send(ws, new MouseReleasePacket(event.button));
 }
 
 window.onwheel = (event) => {
-	if (!connected || !player) return;
-	event.stopPropagation();
-	const delta = event.deltaY < 0 ? -1 : 1;
-	send(ws, new SwitchWeaponPacket(delta));
+  if (!connected || !player) return;
+  event.stopPropagation();
+  const delta = event.deltaY < 0 ? -1 : 1;
+  send(ws, new SwitchWeaponPacket(delta));
 }
 
 window.oncontextmenu = (event) => {
-	if (connected) event.preventDefault();
+  if (connected) event.preventDefault();
 }
 
 window.ondblclick = (event) => {
-	if (connected) event.preventDefault();
+  if (connected) event.preventDefault();
 }
 
 // Because 4 is grenade and it's not done yet
 for (let ii = 0; ii < 3; ii++) {
-	const panel = <HTMLElement> document.getElementById("weapon-panel-" + ii);
-	panel.onmouseenter = panel.onmouseleave = () => toggleMouseDisabled();
-	panel.onclick = () => {
-		if (!connected || !player) return;
-		send(ws, new SwitchWeaponPacket(ii, true));
-	}
+  const panel = <HTMLElement>document.getElementById("weapon-panel-" + ii);
+  panel.onmouseenter = panel.onmouseleave = () => toggleMouseDisabled();
+  panel.onclick = () => {
+    if (!connected || !player) return;
+    send(ws, new SwitchWeaponPacket(ii, true));
+  }
 }
